56c709893b7831f23f35f4112899bf08a579b6c1
Rajkumar <rajtelkar84@gmail.com>
2019-09-21 23:23:45 +0530

    check build is triggering or not

---
+++ src/test/java/testpkg1/Test3.java
@@ -0,0 +1,43 @@
+package testpkg1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	public WebDriver driver;
+	
+	  @Test
+	  public void f() throws InterruptedException {
+		  
+			driver.get("https://login.yahoo.com/");
+					
+			String FBTitle = driver.getTitle();
+			
+			System.out.println(FBTitle);
+	  }
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  
+			System.setProperty("webdriver.chrome.driver", "F:\\New folder\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+		  
+		  driver.quit();
+	  }
+
+
+}
+
+
+
